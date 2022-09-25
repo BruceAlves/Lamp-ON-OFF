6dc6aa1d69947599abea663965975c7f38588e9b
BruceAlves <brucewallace144@gmail.com>
2022-09-25 02:01:19 -0300

    praticando javaScript

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+function acender(){
+    document.querySelector('#imagem').src = 'lampada_ligada.gif'
+    
+}
+
+function apagar(){
+    document.querySelector('#imagem').src = 'lampada_desligada.gif'
+}
